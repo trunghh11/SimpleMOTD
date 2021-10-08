5193b3916fc6096b7e069541b46d207a861aed6e
bambidz <xela0chang@gmail.com>
2021-10-08 20:09:40 +0900

    Delete disambiguate directory

--- disambiguate/run_preprocess.sh
+++
@@ -1,5 +0,0 @@
-python format_disambiguation_data.py \
-	--simmc_train_json="../data/simmc2_dials_dstc10_train.json" \
-	--simmc_dev_json="../data/simmc2_dials_dstc10_dev.json" \
-	--simmc_devtest_json="../data/simmc2_dials_dstc10_devtest.json" \
-	--disambiguate_save_path="./data/"
